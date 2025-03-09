a890f7e7a84a07eb1d40583aa144df9246a0aeb6
Matias Autio <mmatias.autio@gmail.com>
2025-03-09 05:54:31 +0200

    Add react-photo-album and yet-another-react-lightbox

--- components/Gallery.js
+++ components/Gallery.js
@@ -1,30 +1,59 @@
-import Image from 'next/image';
+"use client"
 
-export default async function Gallery(props) {
+import renderLightboxImage from "./helpers/renderLightboxImage";
+import renderGalleryImage from "./helpers/renderGalleryImage";
+
+import { useState } from "react";
+
+import { RowsPhotoAlbum } from "react-photo-album";
+import "react-photo-album/rows.css";
+
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
+
+// import optional lightbox plugins
+// import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
+// import Slideshow from "yet-another-react-lightbox/plugins/slideshow";
+// import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+// import Zoom from "yet-another-react-lightbox/plugins/zoom";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+
+// import photos from "@/components/helpers/photos";
+
+
+
+export default function Gallery(props) {
+  const [index, setIndex] = useState(-1);
+
+  // Transform Contentful images to match expected format
+  const photos = props.images.map((image) => ({
+    src: image.url,
+    width: image.width,
+    height: image.height,
+    alt: image.description || "Image",
+  }));
+
 
   return (
     <>
-      <div>
-        <div className='prose'>
-          <h3>{props.title}</h3>
-          <div className='flex gap-4 overflow-auto w-full scrollbar-hide scroll-smooth'>
-            {props.images.map((image) => (
-              <div key={image.url} className='slide not-prose flex-shrink-0'>
-                <Image
-                  className='object-cover w-64 h-64'
-                  src={image.url}
-                  width={image.width}
-                  height={image.height}
-                  alt={image.description}
-                  sizes="512px"
-                />
-              </div>
-            ))}
-          </div>
-        </div>
-        
-      </div>
+
+      <RowsPhotoAlbum
+        photos={photos}
+        render={{ image: renderGalleryImage }}
+        targetRowHeight={150}
+        defaultContainerWidth={1200}
+        onClick={({ index }) => setIndex(index)}
+      />
+
+      <Lightbox
+        slides={photos}
+        open={index >= 0}
+        index={index}
+        close={() => setIndex(-1)}
+        render={{ slide: renderLightboxImage }}
+      // enable optional lightbox plugins
+      // plugins={[Fullscreen, Slideshow, Thumbnails, Zoom]}
+      />
     </>
   );
 }
-
