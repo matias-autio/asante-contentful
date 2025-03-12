f81bb49bb8befcbc460bfda5b5bd0a2467ca0818
Matias Autio <58674790+matias-autio@users.noreply.github.com>
2025-03-12 18:03:59 +0200

    Add Embed component (#2)

---
+++ components/Embed.js
@@ -0,0 +1,14 @@
+export default async function Embed({ html }) {
+
+    if (!html){
+      return null;
+    }
+
+    return (
+      <>
+        <hr className="border-tertiary"></hr>
+        <div className="max-w-screen-sm mx-auto prose" dangerouslySetInnerHTML={{ __html: html }} />;
+      </>
+    );
+  }
+  
