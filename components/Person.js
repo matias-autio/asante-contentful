0e57bfd342af1b500a453b2e08964fbabe1039d6
Matias Autio <mmatias.autio@gmail.com>
2025-03-21 08:01:43 +0200

    Update Person styling and image sizes

--- components/Person.js
+++ components/Person.js
@@ -4,9 +4,9 @@ import ImageComponent from '@/components/ImageComponent';
 export default async function Person({ person }) {
 
   return (
-    <div className='flex gap-8'>
+    <div className='flex gap-4 md:gap-8'>
       <div className='flex-shrink-0 h-24 w-24 md:w-32 md:h-32 lg:w-48 lg:h-48 bg-tertiary rounded-full'>
-        <ImageComponent className="aspect-square rounded-full h-24 w-24 md:w-32 md:h-32 lg:w-48 lg:h-48 object-cover overflow-hidden" image={person.image} />
+        <ImageComponent sizes="20vw, (min-width: 768px), 10vw" className="aspect-square rounded-full w-full h-full object-cover overflow-hidden" image={person.image} />
       </div>
       <div className='prose'>
         <h4>{person.name}</h4>
